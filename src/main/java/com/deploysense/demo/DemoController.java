6949e066faef04a246c4f28b79f088ea4543594c
Shivaangg Kamleshkumar Patel <shivaanggkl@gmail.com>
2025-12-15 22:21:28 -0600

    comment

--- src/main/java/com/deploysense/demo/DemoController.java
+++ src/main/java/com/deploysense/demo/DemoController.java
@@ -21,7 +21,7 @@ public class DemoController {
         log.error("Error endpoint called");
         if (npe) {
             String s = null;
-            s.length(); // ← deterministic NPE 
+            s.length(); // ← deterministic NPE for demo
         }
         throw new RuntimeException("Intentional demo error");
     }
